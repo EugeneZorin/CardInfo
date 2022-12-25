9b7171638a1b3e81c558c631158ed13cd447dfb7
Eugene Zorin <112591311+EugeneZorin@users.noreply.github.com>
2022-12-25 18:39:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cardinfo/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.cardinfo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val DarkGray = Color(0xFFA9A9A9)
