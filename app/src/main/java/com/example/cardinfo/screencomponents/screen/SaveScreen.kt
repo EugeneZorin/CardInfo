8a759fbb1abde74a2dfe345b918fa38921232933
Eugene Zorin <112591311+EugeneZorin@users.noreply.github.com>
2023-04-16 15:54:07 +0300

    Version 0.88.1 - ex: Adding: 1) Save system in early development 1.0.0

--- app/src/main/java/com/example/cardinfo/screencomponents/screen/SaveScreen.kt
+++ app/src/main/java/com/example/cardinfo/screencomponents/screen/SaveScreen.kt
@@ -1,17 +1,28 @@
 package com.example.cardinfo.screencomponents.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Delete
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -19,7 +30,10 @@ import androidx.navigation.NavHostController
 import com.example.cardinfo.data.constant.ConstantValue.INFORMATION_SAVED_SCREEN
 import com.example.cardinfo.data.constant.ConstantValue.MAIN_SCREEN
 import com.example.cardinfo.viewmodels.room.CardDetailsViewModel
+import kotlin.math.roundToInt
 
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SaveScreen(
     cardDetailsViewModel: CardDetailsViewModel = viewModel(),
@@ -28,34 +42,54 @@ fun SaveScreen(
 
     val inputInfoCard = cardDetailsViewModel.allDetails.observeAsState(emptyList()).value
 
+    val squareSize = 45.dp
+
+    val swipeableState = rememberSwipeableState(0)
+    val sizePx = with(LocalDensity.current) { squareSize.toPx() }
+    val anchors = mapOf(0f to 0, sizePx to 1) // Maps anchor points (in px) to states
+    
     LazyColumn(
         modifier = Modifier.fillMaxSize()
-    ) {
-        item {
+    ){
+        item { 
             inputInfoCard.map {
                 Card(
                     modifier = Modifier
-                        .offset(x = 20.dp, y = 20.dp)
-                        .height(45.dp)
-                        .width(370.dp)
-                        .padding(top = 3.dp)
+                        .background(Color.White)
                         .clickable {
                             navController.navigate("$INFORMATION_SAVED_SCREEN/${it.id}")
-                        },
-                    backgroundColor = Color.Blue,
-                    elevation = 0.dp,
-                    shape = RoundedCornerShape(5.dp)
-                )
-                {
-                    Text(
-                        text = it.DetailsCard,
-                        color = Color.White,
-                        style = TextStyle(fontSize = 15.sp)
-                    )
+                        }
+                        .swipeable(
+                            state = swipeableState,
+                            anchors = anchors,
+                            thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                            orientation = Orientation.Horizontal
+                        ),
+                    backgroundColor = Color.Red
+                ) {
+                    Card(
+                        Modifier
+                            .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) },
+                        backgroundColor = Color.DarkGray,
+
+                    ) {
+                        Text(
+                            text = it.DetailsCard,
+                            Modifier
+                                .fillMaxWidth()
+                                .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
+                                .height(47.dp),
+                            color = Color.White,
+                            style = TextStyle(fontSize = 15.sp)
+                        )
+                    }
                 }
             }
         }
     }
+
+   
+
 
     Box(modifier = Modifier
         .offset(x = 150.dp, y = 570.dp)
