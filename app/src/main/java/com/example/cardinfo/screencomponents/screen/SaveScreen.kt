6ebd6c21bb0432438598b3a9e9648d5025ab49f0
Zubenzitkar <zubenzitkar@gmail.com>
2023-04-24 14:02:04 +0300

    Version 0.88.8: Adding: 1) Rewritten user interface SaveScreen.kt 2) Save system in early development 1.0.0

--- app/src/main/java/com/example/cardinfo/screencomponents/screen/SaveScreen.kt
+++ app/src/main/java/com/example/cardinfo/screencomponents/screen/SaveScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.DismissDirection
@@ -21,6 +22,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,6 +37,7 @@ import androidx.navigation.NavHostController
 import com.example.cardinfo.data.constant.ConstantValue
 import com.example.cardinfo.data.constant.ConstantValue.MAIN_SCREEN
 import com.example.cardinfo.functions.FormatNumberCard
+import com.example.cardinfo.room.CardDetails
 import com.example.cardinfo.viewmodels.room.CardDetailsViewModel
 
 
@@ -44,72 +47,40 @@ fun SaveScreen(
     cardDetailsViewModel: CardDetailsViewModel = viewModel(),
     navController: NavHostController,
 ) {
+
     val inputInfoCard by cardDetailsViewModel.allDetails.observeAsState(emptyList())
-    val updatedCardDetailsState  = rememberUpdatedState(inputInfoCard)
+    val updateItemsState = rememberUpdatedState(inputInfoCard)
 
     // Calling a class to give the card number the correct look
     val formatNumberCard = FormatNumberCard()
 
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ){
-        item ( updatedCardDetailsState.value) {
-            updatedCardDetailsState.value.map { item->
-
-                val dismissState = rememberDismissState (
-                    confirmStateChange = {
-                        if (it == DismissValue.DismissedToEnd) {
-                            cardDetailsViewModel.deleteDetails(item)
-                        }
-                        true
-                    }
-                )
-
-                SwipeToDismiss(
-                    state = dismissState,
-                    dismissThresholds = { FractionalThreshold(0.5f) },
-                    directions = setOf(DismissDirection.StartToEnd),
-                    dismissContent = {
-                        Card(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(65.dp)
-                                .padding(5.dp)
-
-                        ) {
-                            Text(
-                                text = formatNumberCard.formatNumberCard(item.numberCard),
-                                fontSize = 30.sp,
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier.offset(y =  7.dp)
-
-                            )
-                        }
-
-
-                    },
-                    background = { Color.DarkGray },
-                    modifier = Modifier
-                        .clickable {
-                            // Send ID to screen InformationSavedScreen
-                            navController.navigate("${ConstantValue.INFORMATION_SAVED_SCREEN}/${item.id}"
-                        )
-                    },
+        item( inputInfoCard ){
+            updateItemsState.value.map { item->
+                SwipeToDeleteList(
+                    item = item,
+                    onDelete = { cardDetailsViewModel.deleteDetails(item) },
+                    onClick = { navController.navigate("${ConstantValue.INFORMATION_SAVED_SCREEN}/${item.id}")},
+                    formatNumberCard = formatNumberCard,
                 )
             }
         }
     }
 
-
-    LaunchedEffect(updatedCardDetailsState){
+    LaunchedEffect(Unit){
         cardDetailsViewModel.refreshData()
     }
 
-
-    Box(modifier = Modifier
-        .offset(x = 144.dp, y = 690.dp)
-        .fillMaxSize()
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 50.dp),
+        contentAlignment = Alignment.BottomCenter,
     ) {
         Button(
             onClick = { navController.navigate(MAIN_SCREEN) },
@@ -121,7 +92,53 @@ fun SaveScreen(
             Text(text = "Назад")
         }
     }
+}
 
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeToDeleteList(
+    item: CardDetails,
+    onDelete: () -> Unit,
+    onClick: () -> Unit,
+    formatNumberCard: FormatNumberCard,
+) {
+
+    val dismissState = rememberDismissState (
+        confirmStateChange = {
+            if (it == DismissValue.DismissedToEnd) {
+                onDelete()
+            }
+            true
+        }
+    )
+
+    SwipeToDismiss(
+        state = dismissState,
+        dismissThresholds = { FractionalThreshold(0.5f) },
+        directions = setOf(DismissDirection.StartToEnd),
+        dismissContent = {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(65.dp)
+                    .padding(5.dp)
+
+            ) {
+                Text(
+                    text = formatNumberCard.formatNumberCard(item.numberCard),
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.offset(y =  7.dp)
+                )
+            }
+        },
+        background = { Color.DarkGray },
+        modifier = Modifier
+            .clickable {
+                // Send ID to screen InformationSavedScreen
+                onClick()
+            },
+    )
 }
 
 
